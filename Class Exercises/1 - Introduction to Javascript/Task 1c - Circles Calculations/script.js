8dbceab7e3b2c4378137a2da29c4a74153b28507
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-10 19:37:05 +0000

    Q1C NaN Error and Q1D Partial compeletion.

--- Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
@@ -1,6 +1,6 @@
 // Add event listener to the calculate button
 document.getElementById('calculateBtn').addEventListener('click', calculateCircle);
-Math.import
+
 function calculateCircle() {
     // TODO:
     // 1. Get the radius value from the input field
@@ -12,8 +12,8 @@ function calculateCircle() {
 
     let radius = parseInt(document.getElementById('radius'));
 
-    let circumference = 2 * Math.PI * radius;
-    let area = Math.PI * radius ^ 2;
+    let circumference = 2 * 3.17 * radius;
+    let area = 3.17 * radius ^ 2;
 
     document.getElementById('circumference').textContent = circumference
     document.getElementById('area').textContent = area
--- Class Exercises/1 - Introduction to Javascript/Task 1d - Whats the Hypotenuse/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1d - Whats the Hypotenuse/script.js
@@ -1,10 +1,20 @@
 // Add event listener to the calculate button
 document.getElementById('calculateBtn').addEventListener('click', calculateHypotenuse);
 
+
+
 function calculateHypotenuse() {
     // TODO:
     // 1. Get the lengths of sides A and B from the input fields
     // 2. Calculate the hypotenuse using the Pythagorean theorem: c = √(a² + b²)
     // 3. Display the result in the hypotenuse span element
     // Note: Use Math.sqrt() for square root
+
+    let sideA = parseFloat(document.getElementById('sideA').value);
+    let sideB = parseFloat(document.getElementById('sideB').value);
+
+    let hypotenuse =  Math.sqrt(sideA + sideB);
+
+    document.getElementById('hypotenuse').textContent = hypotenuse
+
 }
