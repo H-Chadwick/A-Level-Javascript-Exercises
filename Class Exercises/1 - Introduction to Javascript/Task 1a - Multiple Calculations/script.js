a214f27a13b9f4aac2d8cf8466d2b9382fe59831
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-10 08:49:32 +0000

    Working on Excersie 1

--- Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
@@ -9,4 +9,29 @@ function calculate() {
     //    - A × B × C
     //    - A + (B × C)
     // 3. Display the results in the respective span elements
+
+    let num1 = parseFloat(document.getElementById('numberA').value);
+    let num2 = parseFloat(document.getElementById('numberB').value);
+    let num3 = parseFloat(document.getElementById('numberC').value);
+
+    average = (num1 * num2 * num3) / 3;
+    product = num1 * num2 * num3 ;
+    expression = num1 + (num2 * num3);
+
+    if (isNaN(num1) || isNaN(num2) || (num3)) {
+        document.getElementById('result').textContent = "Please enter valid numbers";
+    } else {
+        const average = (num1 * num2 * num3) / 3;
+        const product = num1 * num2 * num3;
+        const expression = num1 + (num2 * num3);
+        document.getElementById('average').textContent = average;
+        document.getElementById('product').textContent = product;
+        document.getElementById('expression').textContent = expression;
+    }
+}
+
+
+
+
+
 }
