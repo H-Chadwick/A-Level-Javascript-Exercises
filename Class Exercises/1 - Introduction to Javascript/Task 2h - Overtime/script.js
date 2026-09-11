5941564f24996705d5df3a54d2d7dadac6cfbe25
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-11 17:50:11 +0000

    Q2H ***NOT TESTED***

--- Class Exercises/1 - Introduction to Javascript/Task 2h - Overtime/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2h - Overtime/script.js
@@ -13,4 +13,18 @@ function calculatePay() {
     //    - Overtime pay (overtime rate × overtime hours)
     //    - Total pay (basic pay + overtime pay)
     // 3. Display all amounts formatted to 2 decimal places
+
+    let standardRate = parseFloat(document.getElementById('standardRate').value);
+    let overtimeRate = parseFloat(document.getElementById('overtimeRate').value);
+    let basicHours = parseFloat(document.getElementById('basicHours').value);
+    let overtimeHours = parseFloat(document.getElementById('overtimeHours').value);
+
+    let basicPay = (standardRate * basicHours);
+    let overtimePay = (overtimeRate * overtimeHours);
+    let totalPay = (basicPay + overtimePay);
+
+    document.getElementById('basicPay').textContent = basicPay;
+    document.getElementById('overtimePay').textContent = overtimePay;
+    document.getElementById('totalPay').textContent = totalPay;
+
 }
