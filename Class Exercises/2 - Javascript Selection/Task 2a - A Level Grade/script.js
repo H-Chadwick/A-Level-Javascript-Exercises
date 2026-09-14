1adb26d78bcc782556bed0224a94187265394c24
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-14 20:16:57 +0000

    A-LEVEL GRADE COMPLETED AND TESTED

--- Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
@@ -10,11 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the A-Level grade
 function calculateGrade() {
     // TODO: Get the marks for all four components
-    
+    let comp1 = parseInt(document.getElementById('comp1').value);
+    let comp2 = parseInt(document.getElementById('comp2').value);
+    let comp3 = parseInt(document.getElementById('comp3').value);
+    let comp4 = parseInt(document.getElementById('comp4').value);
     // TODO: Calculate the total marks
-    
+    let total = comp1 + comp2 + comp3 + comp4
     // TODO: Calculate the percentage (total possible marks is 400)
-    
+    let percentage = (total / 400) * 100
     // TODO: Determine the grade based on the percentage
     // A: 80% or greater
     // B: 70% or greater
@@ -22,6 +25,21 @@ function calculateGrade() {
     // D: 50% or greater
     // E: 40% or greater
     // U: Less than 40%
-    
+    let result = ''
+    if (percentage >= 80) {
+        result = 'A'
+    } else if (percentage >=70) {
+        result = 'B'
+    } else if (percentage >=60) {
+        result = 'C'
+    } else if (percentage >=50) {
+        result = 'D'
+    } else if (percentage >=40) {
+        result = 'E'
+    } else if (percentage <40) {
+        result = 'U'
+    }
     // TODO: Display the percentage and grade
+    document.getElementById('percentage').textContent = percentage;
+    document.getElementById('result').textContent = result;
 }
