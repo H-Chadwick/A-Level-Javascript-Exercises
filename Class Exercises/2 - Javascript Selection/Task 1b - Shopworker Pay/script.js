dda5ec9275561635c303b08b129e47c0c55eb5bf
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-14 11:56:24 +0000

    PART 2 - Q1B COMPLETE WORKING

--- Class Exercises/2 - Javascript Selection/Task 1b - Shopworker Pay/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1b - Shopworker Pay/script.js
@@ -10,10 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the worker's pay
 function calculatePay() {
     // TODO: Get the age and hours worked from the input fields
-    
+    let age = parseInt(document.getElementById('age').value);
+    let hours = parseInt(document.getElementById('hours').value);
+    let hourlyRate = 0;
     // TODO: Set the hourly rate based on age (£4.85 for under 18, £6.35 for 18 and over)
-    
+    if (age < 18) {
+        hourlyRate = 4.85;
+    } else {
+        hourlyRate = 6.35;
+    }
     // TODO: Calculate total pay
-    
+    let total = hours * hourlyRate;
     // TODO: Display the hours worked and total amount earned
+    console.log(`age = ${age}`)
+    console.log(`hours ${hours}`)
+    console.log(`hourlyRate ${hourlyRate}`)
+    console.log(`total ${total}`)
+    document.getElementById('result').textContent = total;
 }
