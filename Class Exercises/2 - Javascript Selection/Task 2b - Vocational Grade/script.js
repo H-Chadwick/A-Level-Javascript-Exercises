c90b9f25685c4d5d873282695e5b17b165c2341e
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-14 20:25:54 +0000

    VOCATIONAL CERTIFICATE COMPLETE

--- Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
@@ -10,11 +10,24 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the Hang Gliding Certificate grade
 function calculateGrade() {
     // TODO: Get the theory and practical exam scores
-    
+    let theory = parseInt(document.getElementById('theory').value);
+    let practical = parseInt(document.getElementById('practical').value);
+    let percentage = theory + practical;
     // TODO: Check if both scores are above 50% (Pass requirement)
-    
+    if (percentage >= 50) {
+        let result = 'Passed'
+    } else {
+        result = 'Failed'
+    }
     // TODO: If passed, calculate average and check if above 70% (Distinction requirement)
-    
+    if (percentage >= 70) {
+        result = 'Distinciton'
+    }
     // TODO: Display the appropriate grade (Pass, Distinction, or Fail)
     // Also display the average score
+
+    let average = (percentage) / 2
+
+    document.getElementById('result').textContent = result;
+    document.getElementById('average').textContent = average;
 }
