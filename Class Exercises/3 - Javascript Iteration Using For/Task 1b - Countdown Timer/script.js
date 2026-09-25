c08eca8e5b34ce5873babbf2de235990d4c02da3
H-Chadwick <628hc21@canonslade.tbft.uk>
2026-09-25 09:21:28 +0000

    ***COUNTDOWN COMPLETE***

--- Class Exercises/3 - Javascript Iteration Using For/Task 1b - Countdown Timer/script.js
+++ Class Exercises/3 - Javascript Iteration Using For/Task 1b - Countdown Timer/script.js
@@ -13,18 +13,33 @@ function startCountdown() {
     // Check if start number is positive
     // Check if delay is at least 100ms
     if (isNaN(startNum) || isNaN(delay)) {
-        display.innerHTML = "Check numbers are valid"
-    } else if (start >=1) {
-        display.innerHTML = "Start is positive"
-    } else if ()
+        display.textContent = 'Please enter valid numbers.';
+        return;
+    }
+    if (startNum <= 0) {
+        display.textContent = 'Start number must be a positive integer.';
+        return;
+    }
+    if (delay < 100) {
+        display.textContent = 'Delay must be at least 100ms.';
+        return;
+    }
     // TODO: Disable button during countdown
-    
+    button.disabled = true;
     // TODO: Create the countdown loop
     // Use a for loop counting backwards
     // Use setTimeout to create the delay between numbers
-    
+    for (let i = startNum; i >= 0; i--) {
+        setTimeout(() => {
+            display.textContent = i;
+            if (i === 0) {
+                display.textContent = 'Blast off!';
+                button.disabled = false; // Re-enable button after countdown finishes
+            }
+        }, (startNum - i) * delay);
+    }       
     // TODO: Show "Blast off!" at the end
-    
+                                         
     // TODO: Re-enable button after countdown finishes
 }
 
